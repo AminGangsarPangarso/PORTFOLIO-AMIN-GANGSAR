45119afc217279e8933a42d10b65037bf1de5ceb
AminGangsarPangarso <amindailylife1976@gmail.com>
2023-10-18 14:56:10 +0700

    halaman about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,65 @@
 import React from 'react';
-
+import Countup from 'react-countup'
+import { useInView } from 'react-intersection-observer';
+import { motion } from "framer-motion"
+import { fadein } from '../variants'
 const About = () => {
-  return(
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  })
+  return (
+    <section className=" section" id='about' ref={ref}>
 
-   <div className=" section" id='about'>
-      
-      About
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+          {/* img */}
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten   bg-top"></div>
+          {/* text */}
+          <div className="flex-1">
+            <h2 className='h2 text-accent'>About me</h2>
+            <h3 className='h3 mb-4'>I'm a Front-end Developer web with over 3 years of experience</h3>
+            <p className='mb-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, libero sed dolorem perferendis expedita quo voluptatum at velit labore! Tenetur!</p>
+          </div>
 
-   </div>
+          <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+            <div className="">
+              <div className=" text-[40px] font-tertiary text-gradient mb-2">
+                {
+                  inView ?
+                    <Countup start={0} end={3} duration={3} /> : null}
+              </div>
+              <div className="font-primary text-sm traking-[2px]">Years of <br /> Experinece</div>
+            </div>
+            <div className="">
+              <div className=" text-[40px] font-tertiary text-gradient mb-2">
+                {
+                  inView ?
+                    <Countup start={0} end={10} duration={3} /> : null}
+                k+
+              </div>
+              <div className="font-primary text-sm traking-[2px]">Project<br /> Complate</div>
+            </div>
+            <div className="">
+              <div className=" text-[40px] font-tertiary text-gradient mb-2">
+                {
+                  inView ?
+                    <Countup start={0} end={13} duration={3} /> : null}
+                k+
+              </div>
+              <div className="font-primary text-sm traking-[2px]">Satisfied <br /> Client</div>
+            </div>
+            
+          </div>
+          <div className="mt-6 flex gap-x-8 items-center">
+            <button className='btn btn-sm lg:btn'>Contact me</button>
+            <a href="#" className='text-gradient btn-link'>
+              My Portofolio
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </section>
 
 
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,40 +1,98 @@
 import React from 'react';
 import image from '../assets/avatar.svg'
-// import {BsGithub} from 'react-icons/bs'
-// import {AiFillLinkedin} from 'react-icons/ai'
-// import {FaInstagramSquare} from 'react-icons/fa'
+import { BsGithub } from 'react-icons/bs'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { FaInstagramSquare } from 'react-icons/fa'
 import { TypeAnimation } from 'react-type-animation';
 import { fadeIn } from '../variants';
+import { motion } from 'framer-motion';
 
 const Banner = () => {
-  return <div className='section ' id='home'>
-    <div className="container mx-auto flex "> 
-      <div className=" flex flex-col">
-        <span className='font-bold text text-4xl'>Amin {''}</span>
-      </div>
-      <div className="">
-        <span className='font-bold text text-4xl'>Im a</span>
-          <TypeAnimation
-          sequence={[
-            // Same substring at the start will only be typed out once, initially
-            'Web Developer',
-            9000, // wait 1s before replacing "Mice" with "Hamsters"
-            'UI/UX DESIGNER',
-          
-          ]}
-          wrapper="span"
-          className='text-chan'
-          speed={50}
-          style={{ fontSize: '2em', display: 'inline-block' }}
-          repeat={Infinity}
-          />
-      </div>
+  return (
 
-    <div className="">
-      <img src={image} alt="" className='w-52 h-52' />
+    <section className='section  min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+      <div className="container mx-auto   ">
+        <div className="  flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12  ">
+          <div className=" flex-1 text-center font-secondary lg:text-left ">
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+              Amin {''}<span className=''>Gangsar </span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mt-1 mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+              <span className='font-bold text text-[55px mr-4 text-white
+              '>Im a</span>
+              <TypeAnimation
+                sequence={[
+                  'web developer',
+                  2000,
+                  'FRONTEND DEVELOPER',
+                  2000,
+                ]}
+                wrapper="span"
+                className='text-accent'
+                speed={50}
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam temporibus blanditiis neque aut non. Numquam pariatur eum architecto voluptates obcaecati? Suscipit nisi temporibus ex pariatur officia voluptatibus quidem ipsam non?</motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className='btn btn-lg mr-2 '>Contact me</button>
+              <a href="#" className='text-gradient btn-link'>
+                My Portofilio
+              </a>
+            </motion.div>
+
+
+            <motion.div
+             variants={fadeIn('up',0.3)}
+             initial='hidden'
+             whileInView={'show'}
+             viewport={{once:false, amount:0.7}}
+            className="flex gap-x-6 items-center  py-5 text-[20px] max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaInstagramSquare />
+              </a>
+              <a href="#">
+                <BsGithub />
+              </a>
+              <a href="#">
+                <AiFillLinkedin />
+              </a>
+            </motion.div>
+
+
+          </div>
+          <motion.div
+           variants={fadeIn('down',0.3)}
+           initial='hidden'
+           whileInView={'show'}
+          className=" hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] ">
+            <img src={image} alt="" className='w-[500px] h-[500px]' />
+          </motion.div>
+        </div>
       </div>
-    </div>
-    </div>;
+    </section>
+
+  )
 };
 
 export default Banner;
