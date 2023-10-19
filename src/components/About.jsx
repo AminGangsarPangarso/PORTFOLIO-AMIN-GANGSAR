cadcf39bd10ef1edea9de0f5ddca8097736d799f
AminGangsarPangarso <amindailylife1976@gmail.com>
2023-10-19 14:29:42 +0700

    halaman services

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Countup from 'react-countup'
 import { useInView } from 'react-intersection-observer';
+import { fadeIn } from '../variants';
 import { motion } from "framer-motion"
-import { fadein } from '../variants'
 const About = () => {
   const [ref, inView] = useInView({
     threshold: 0.5,
@@ -13,13 +13,25 @@ const About = () => {
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten   bg-top"></div>
+          <motion.div
+            variants={fadeIn ('right',0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten   bg-top">
+
+          </motion.div>
           {/* text */}
-          <div className="flex-1">
+          <motion.div
+            variants={fadeIn ('left', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1">
             <h2 className='h2 text-accent'>About me</h2>
             <h3 className='h3 mb-4'>I'm a Front-end Developer web with over 3 years of experience</h3>
             <p className='mb-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, libero sed dolorem perferendis expedita quo voluptatum at velit labore! Tenetur!</p>
-          </div>
+          </motion.div>
 
           <div className="flex gap-x-6 lg:gap-x-10 mb-12">
             <div className="">
@@ -48,7 +60,7 @@ const About = () => {
               </div>
               <div className="font-primary text-sm traking-[2px]">Satisfied <br /> Client</div>
             </div>
-            
+
           </div>
           <div className="mt-6 flex gap-x-8 items-center">
             <button className='btn btn-sm lg:btn'>Contact me</button>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,95 @@
 import React from 'react';
+import { BsArrowRight } from 'react-icons/bs'
 
+import { fadeIn } from '../variants';
+import { motion } from 'framer-motion';
 const Services = () => {
-  return <div className='section' id='services'>Services</div>;
+
+  const services = [
+    {
+      id: 1,
+      name: 'Web Developeer',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolore, aperiam repellat illum quod repudiandae id a dolorum qui quis.',
+      link: 'lorem fsdfadfd'
+    },
+    {
+      id: 2,
+      name: 'Web Developeer',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolore, aperiam repellat illum quod repudiandae id a dolorum qui quis.',
+      link: 'lorem fsdfadfd'
+    },
+    {
+      id: 3,
+      name: 'Web Developeer',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolore, aperiam repellat illum quod repudiandae id a dolorum qui quis.',
+      link: 'lorem fsdfadfd'
+    },
+    {
+      id: 4,
+      name: 'UI UX Designer',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolore, aperiam repellat illum quod repudiandae id a dolorum qui quis.',
+      link: 'lorem fsdfadfd'
+    }
+  ]
+
+
+
+  return (
+    <section className='section' id='services'>
+
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text */}
+          <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat bg-blend-lighten bg-opacity-100  mb-12 lg:mb-0 ">
+            <h1 className='h2 text-accent mb-5'>What I DO</h1>
+            <h3 className='h3 max-w-[450px] mb-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, eaque ipsa recusandae atque optio numquam minima est voluptatum fuga minus?</h3>
+
+            <button className='btn btn-sm'>See my work</button>
+          </motion.div>
+          {/* services */}
+          <motion.div
+           variants={fadeIn('left', 0.3)}
+           initial='hidden'
+           whileInView={'show'}
+           viewport={{ once: false, amount: 0.7 }}
+          className="flex-1">
+            {/* service list */}
+            <div className="">
+              {services.map((item) => {
+
+                return (
+                  <div key={item.id} className="border-b border-white/20 h-[146px]">
+                    <div className="max-w-[476px] mb-[38px]">
+                      <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{item.name}</h4>
+                      <p className='font-secondary leading-tight'>{item.description}</p>
+                    </div>
+                    <div className="flex flex-col flex-1 items-end">
+                      <a href="" className='btn w-9 h-9 mb-[24px] flex justify-center items-center'>
+                        <BsArrowRight/>
+                      </a>
+                      <a href="" className='text-gradient text-sm'>{item.link}</a>
+                    </div>
+                  </div>
+                )
+
+              })}
+            </div>
+          </motion.div>
+        </div>
+
+      </div>
+
+    </section>
+
+
+
+  )
+
 };
 
 export default Services;
